856578c97c6eb5cdd8496a768ea1eda8eb236163
Sergey <sergsmile29@gmail.com>
2024-02-14 23:59:17 +0200

    init m2l2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
-import { Counter } from './components/Counter/Counter'
-import { TodoList } from './components/TodoList/TodoList'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import { StyledPreview } from './components/StyledPreview/StyledPreview'
-import colors from './assets/colors.json'
+import { RegisterForm } from './components/RegisterForm/RegisterForm'
+import { Employee } from './components/Employees/Employee'
 const App = () => {
 	return (
-		<div>
-			{/* <StyledPreview /> */}
-			{/* <Counter /> */}
-			<TodoList />
-			{/* <ColorPicker colors={colors} message='Hello' /> */}
-		</div>
+		<>
+			<RegisterForm />
+			<Employee />
+		</>
 	)
 }
 
