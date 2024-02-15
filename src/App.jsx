dcbd6e1cfea2f416d1de49ea3d11d0415f7b78c5
Sergey <sergsmile29@gmail.com>
2024-02-15 10:14:45 +0200

    uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { RegisterForm } from './components/RegisterForm/RegisterForm'
 import { Employee } from './components/Employees/Employee'
+import './index.css'
+import { RegisterFormUncontrolled } from './components/RegisterForm/RegisterFormUncontrolled'
 const App = () => {
+	const handleRegister = data => {
+		console.log('Send data...')
+		console.log('...')
+		setTimeout(() => {
+			console.log(data)
+			console.log('register is success')
+		}, 2000)
+	}
 	return (
 		<>
-			<RegisterForm />
-			<Employee />
+			<RegisterFormUncontrolled register={handleRegister} />
+			{/* <Employee /> */}
 		</>
 	)
 }
