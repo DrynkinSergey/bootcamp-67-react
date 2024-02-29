935864e5722a7d75fb4de3c1fb5fb36229ab55c2
Sergey <sergsmile29@gmail.com>
2024-02-29 10:04:32 +0200

    createAction

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,17 +1,22 @@
 // Action creator
 
+import { createAction } from '@reduxjs/toolkit'
 import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from './constants'
 
-export const increment = () => ({ type: INCREMENT })
-export const decrement = () => ({ type: DECREMENT })
-export const reset = () => ({ type: RESET })
-export const changeStep = SUPER_PUPER_ARGUMENT_FOR_MY_FN_REDUX => ({
-	type: CHANGE_STEP,
-	payload: SUPER_PUPER_ARGUMENT_FOR_MY_FN_REDUX,
-})
+// export const increment = () => ({ type: INCREMENT })
+// export const decrement = () => ({ type: DECREMENT })
+// export const reset = () => ({ type: RESET })
+// export const changeStep = SUPER_PUPER_ARGUMENT_FOR_MY_FN_REDUX => ({
+// 	type: CHANGE_STEP,
+// 	payload: SUPER_PUPER_ARGUMENT_FOR_MY_FN_REDUX,
+// })
 
-// const sum = (a, b) => a + b
+export const increment = createAction('increment')
+export const decrement = createAction('decrement')
+export const reset = createAction('reset')
+export const changeStep = createAction('changeStep')
 
-// sum(1, 3)
-// sum(33, 22)
-// sum('hello', 'world')
+// console.log(increment())
+// console.log(increment(123))
+// console.log(increment('123'))
+// console.log(increment([1, 2, 3, 4, 5, 6]))
--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,3 +1,4 @@
+import { changeStep, decrement, increment, reset } from './actions'
 import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from './constants'
 
 const initialState = {
@@ -7,23 +8,23 @@ const initialState = {
 
 export const counterReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case INCREMENT:
+		case increment.type:
 			return {
 				...state,
 				counter: state.counter + state.step,
 			}
-		case DECREMENT:
+		case decrement.type:
 			return {
 				...state,
 				counter: state.counter - state.step,
 			}
-		case RESET:
+		case reset.type:
 			return {
 				...state,
 				counter: 1,
 				step: 1,
 			}
-		case CHANGE_STEP:
+		case changeStep.type:
 			return {
 				...state,
 				step: action.payload,
