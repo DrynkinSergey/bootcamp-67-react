db617b2ce4ceb708e21b5c84c8e6e48966d3b2ae
Sergey <sergsmile29@gmail.com>
2024-03-04 10:18:26 +0200

    middleware

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import { Counter } from './components/Counter/Counter'
 import { TodoList } from './components/TodoList/TodoList'
 
 export const App = () => {
 	return (
 		<div>
-			{/* <Counter /> */}
 			<TodoList />
 		</div>
 	)
